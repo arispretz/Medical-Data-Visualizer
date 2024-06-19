eee14b78e67769ef192534f5136b59ba0921916d
arispretz <117314460+arispretz@users.noreply.github.com>
2024-06-18 22:20:29 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const fastify = require('fastify')({ logger: true });
+const path = require('path');
+
+// Configurar Fastify para servir archivos estáticos
+fastify.register(require('@fastify/static'), {
+  root: path.join(__dirname, 'static'),
+  prefix: '/static/',
+});
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen(3000, '0.0.0.0');
+    fastify.log.info(`server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
